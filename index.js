877f823542c6bb755996405fbd0025f97eeee330
CharlesCoeder <croederdev@gmail.com>
2023-12-25 23:09:00 -0500

    add join function

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ path.sep
 
 function _normalize(path) {
   valid(path);
-  return `${path}/`.split(/\/+/g).reduce((previous, current) => {
-    if (!Array.isArray(previous)) previous = [previous];
-    if (current === '.') return previous;
-    if (current === '..') previous.pop();
-    else if (current.length) previous.push(current);
-    return previous;
-  }).join('/');
+  return `${path}/`
+    .split(/\/+/g)
+    .reduce((previous, current) => {
+      if (!Array.isArray(previous)) previous = [previous];
+      if (current === ".") return previous;
+      if (current === "..") previous.pop();
+      else if (current.length) previous.push(current);
+      return previous;
+    })
+    .join("/");
 }
 
 function _basename(path, ext) {
@@ -29,72 +32,68 @@ function _basename(path, ext) {
     var tmp = basename.split(/\./g);
     var _ext = tmp.pop();
     if (ext === _ext || ext.slice(1) === _ext) {
-      return tmp.join('.')
+      return tmp.join(".");
     }
   }
   return basename;
 }
 
 function _dirname(path) {
-  return path.split(/\//g).slice(0, -1).join('/')
+  return path.split(/\//g).slice(0, -1).join("/");
 }
 
 function _extname(path) {
-  var tmp = path.replace(/^[\.]+/, '');
+  var tmp = path.replace(/^[\.]+/, "");
   if (/\./.test(tmp)) return tmp.match(/\.[^.]*$/)[0];
-  return '';
+  return "";
 }
 
 function _format(options) {
-  var {
-    dir,
-    root,
-    base,
-    name,
-    ext
-  } = options;
+  var { dir, root, base, name, ext } = options;
   var _dir = dir || root;
-  var _base = base || `${name || ''}${/^\./.test(ext) ? '' : '.'}${ext || ''}`;
+  var _base = base || `${name || ""}${/^\./.test(ext) ? "" : "."}${ext || ""}`;
   return normalize(`${_dir}/${_base}`);
 }
 
 function _isAbsolute(path) {
-  return /^\//.test(path)
+  return /^\//.test(path);
 }
 
 function _parse(path) {
   var obj = {},
     tmp;
-  var components = path.split(/\//g)
+  var components = path.split(/\//g);
   obj.base = components.pop();
-  obj.dir = components.join('/');
+  obj.dir = components.join("/");
   if (/^\//.test(obj.dir)) {
-    obj.root = '/';
+    obj.root = "/";
   }
   if (obj.base != undefined) {
-    tmp = obj.base.replace(/^[\.]+/, '');
+    tmp = obj.base.replace(/^[\.]+/, "");
     if (/\./.test(tmp)) {
       obj.ext = tmp.match(/\.[^.]*$/)[0];
       obj.name = obj.base.slice(0, -obj.ext.length);
     } else {
-      obj.name = obj.base
+      obj.name = obj.base;
     }
   } else {
-    delete obj.base
+    delete obj.base;
   }
   return obj;
 }
 
 function _resolve(segments) {
-  var flat = segments.reduce((previous, current) => {
-    if (!Array.isArray(previous)) previous = [previous];
-    if (Array.isArray(current)) Array.prototype.push.apply(previous, current);
-    Array.prototype.push.call(previous, current);
-    return previous;
-  }).reduce((previous, current) => {
-    if (/^\//.test(current)) return current;
-    return `${previous}/${current}`;
-  });
+  var flat = segments
+    .reduce((previous, current) => {
+      if (!Array.isArray(previous)) previous = [previous];
+      if (Array.isArray(current)) Array.prototype.push.apply(previous, current);
+      Array.prototype.push.call(previous, current);
+      return previous;
+    })
+    .reduce((previous, current) => {
+      if (/^\//.test(current)) return current;
+      return `${previous}/${current}`;
+    });
   return _normalize(flat);
 }
 
@@ -102,20 +101,30 @@ function _relative(base, path) {
   base = base.split(/\//g);
   path = path.split(/\//g);
 
-  while(base[0] === path[0]) {
+  while (base[0] === path[0]) {
     base.shift();
     path.shift();
   }
 
-  return Array(base.length).fill('..').concat(path).join('/');
+  return Array(base.length).fill("..").concat(path).join("/");
+}
+
+function _join(...paths) {
+  return paths
+    .map((part, index) => {
+      const isLastPart = index === paths.length - 1;
+      let formattedPart = part.endsWith("/") ? part.slice(0, -1) : part;
+      if (!isLastPart && formattedPart.length > 0) formattedPart += "/";
+      return formattedPart;
+    })
+    .join("");
 }
 
 function valid(path) {
-  if (typeof path != 'string') {
-    throw new TypeError('path must be string.');
+  if (typeof path != "string") {
+    throw new TypeError("path must be string.");
   }
 }
-
 
 module.exports = exports = {
   normalize(path) {
@@ -128,21 +137,24 @@ module.exports = exports = {
     return _dirname(_normalize(path));
   },
   extname(path) {
-    return _extname(_normalize(path))
+    return _extname(_normalize(path));
   },
   format(options) {
     return _format(options);
   },
   isAbsolute(path) {
-    return _isAbsolute(valid(path))
+    return _isAbsolute(valid(path));
   },
   parse(path) {
-    return _parse(_normalize(path))
+    return _parse(_normalize(path));
   },
   resolve() {
     return _resolve.call({}, Array.prototype.slice.call(arguments, 0));
   },
   relative(base, path) {
-    return _relative(_normalize(base), _normalize(path))
-  }
-}
+    return _relative(_normalize(base), _normalize(path));
+  },
+  join(...paths) {
+    return _join(...paths);
+  },
+};
